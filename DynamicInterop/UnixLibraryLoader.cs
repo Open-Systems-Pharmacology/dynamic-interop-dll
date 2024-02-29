c590ad3a0afd7d4cbf57395542166c297d8f87c8
Robert McIntosh <261477+rwmcintosh@users.noreply.github.com>
2024-02-29 13:11:00 -0500

    PR feedback

--- DynamicInterop/UnixLibraryLoader.cs
+++ DynamicInterop/UnixLibraryLoader.cs
@@ -1,132 +1,126 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
 using System.Runtime.ConstrainedExecution;
 using System.Runtime.InteropServices;
 using System.Security.Permissions;
-using System.Text;
 
 namespace DynamicInterop
 {
-    [SecurityPermission(SecurityAction.Demand, Flags = SecurityPermissionFlag.UnmanagedCode)]
-    internal class UnixLibraryLoader : IDynamicLibraryLoader
-    {
-        public IntPtr LoadLibrary(string filename)
-        {
-            const int RTLD_LAZY = 0x1;
+   [SecurityPermission(SecurityAction.Demand, Flags = SecurityPermissionFlag.UnmanagedCode)]
+   internal class UnixLibraryLoader : IDynamicLibraryLoader
+   {
+      public IntPtr LoadLibrary(string filename)
+      {
+         const int RTLD_LAZY = 0x1;
 
-            if (_so == 0)
-                return InternalLoadLibrary(filename, RTLD_LAZY);
+         if (_so == 0)
+            return InternalLoadLibrary(filename, RTLD_LAZY);
 
-            if (_so == 1)
-                return dlopen1(filename, RTLD_LAZY);
-            else
-                return dlopen2(filename, RTLD_LAZY);
-        }
+         if (_so == 1)
+            return dlopen1(filename, RTLD_LAZY);
 
-        /// <summary>
-        /// Gets the last error. NOTE: according to http://tldp.org/HOWTO/Program-Library-HOWTO/dl-libraries.html, returns NULL if called more than once after dlopen.
-        /// </summary>
-        /// <returns>The last error.</returns>
-        public string GetLastError()
-        {
-            if (_so == 0)
-                throw new Exception("Called GetLastError before LoadLibrary");
+         return dlopen2(filename, RTLD_LAZY);
+      }
 
-            if (_so == 1)
-                return dlerror1();
-            else
-                return dlerror2();
-        }
+      /// <summary>
+      ///    Gets the last error. NOTE: according to http://tldp.org/HOWTO/Program-Library-HOWTO/dl-libraries.html, returns NULL
+      ///    if called more than once after dlopen.
+      /// </summary>
+      /// <returns>The last error.</returns>
+      public string GetLastError()
+      {
+         if (_so == 0)
+            throw new Exception(checkLoadLibraryStatus("GetLastError"));
 
-        /// <summary>
-        /// Unloads a library
-        /// </summary>
-        /// <param name="handle">The pointer resulting from loading the library</param>
-        /// <returns>True if the function dlclose returned 0</returns>
-        public bool FreeLibrary(IntPtr handle)
-        {
-            if (_so == 0)
-                throw new Exception("Called GetLastError before LoadLibrary");
+         if (_so == 1)
+            return dlerror1();
 
-            // according to the manual page on a Debian box
-            // The function dlclose() returns 0 on success, and nonzero on error.
-            if (_so == 1)
-            {
-                var status = dlclose1(handle);
-                return status == 0;
-            }
-            else
-            {
-                var status = dlclose2(handle);
-                return status == 0;
-            }
-        }
+         return dlerror2();
+      }
 
-        public IntPtr GetFunctionAddress(IntPtr hModule, string lpProcName)
-        {
-            if (_so == 0)
-                throw new Exception("Called GetLastError before LoadLibrary");
+      private static string checkLoadLibraryStatus(string callingFunctionName)
+      {
+         return $"Called {callingFunctionName} before LoadLibrary";
+      }
 
-            if (_so == 1)
-                return dlsym1(hModule, lpProcName);
-            else
-                return dlsym2(hModule, lpProcName);
-        }
+      /// <summary>
+      ///    Unloads a library
+      /// </summary>
+      /// <param name="handle">The pointer resulting from loading the library</param>
+      /// <returns>True if the function dlclose returned 0</returns>
+      public bool FreeLibrary(IntPtr handle)
+      {
+         if (_so == 0)
+            throw new Exception(checkLoadLibraryStatus("FreeLibrary"));
 
-        internal static IntPtr InternalLoadLibrary(string filename, int lazy)
-        {
-            try
-            {
-                var result = dlopen1(filename, lazy);
-                _so = 1;
-                return result;
-            }
-            catch (DllNotFoundException)
-            {
-                _so = 2;
-            }
+         // according to the manual page on a Debian box
+         // The function dlclose() returns 0 on success, and nonzero on error.
+         if (_so == 1)
+         {
+            var status = dlclose1(handle);
+            return status == 0;
+         }
+         else
+         {
+            var status = dlclose2(handle);
+            return status == 0;
+         }
+      }
 
-            return dlopen2(filename, lazy);
-        }
+      public IntPtr GetFunctionAddress(IntPtr hModule, string lpProcName)
+      {
+         if (_so == 0)
+            throw new Exception(checkLoadLibraryStatus("GetFunctionAddress"));
 
-        static List<string> getSearchPaths(string pathsEnvVar)
-        {
-            var searchPaths = (Environment.GetEnvironmentVariable (pathsEnvVar) ?? "").Split (Path.PathSeparator).ToList ();
-            return searchPaths;
-        }
+         if (_so == 1)
+            return dlsym1(hModule, lpProcName);
 
-        private static int _so = 0;
+         return dlsym2(hModule, lpProcName);
+      }
 
-        [DllImport("libdl.so", EntryPoint = "dlopen")]
-        private static extern IntPtr dlopen1([MarshalAs(UnmanagedType.LPStr)] string filename, int flag);
+      internal static IntPtr InternalLoadLibrary(string filename, int lazy)
+      {
+         try
+         {
+            var result = dlopen1(filename, lazy);
+            _so = 1;
+            return result;
+         }
+         catch (DllNotFoundException)
+         {
+            _so = 2;
+         }
 
-        [DllImport("libdl.so.2", EntryPoint = "dlopen")]
-        private static extern IntPtr dlopen2([MarshalAs(UnmanagedType.LPStr)] string filename, int flag);
+         return dlopen2(filename, lazy);
+      }
 
+      private static int _so;
 
-        [DllImport("libdl.so", EntryPoint = "dlerror")]
-        [return: MarshalAs(UnmanagedType.LPStr)]
-        private static extern string dlerror1();
+      [DllImport("libdl.so", EntryPoint = "dlopen")]
+      private static extern IntPtr dlopen1([MarshalAs(UnmanagedType.LPStr)] string filename, int flag);
 
-        [DllImport("libdl.so.2", EntryPoint = "dlerror")]
-        [return: MarshalAs(UnmanagedType.LPStr)]
-        private static extern string dlerror2();
+      [DllImport("libdl.so.2", EntryPoint = "dlopen")]
+      private static extern IntPtr dlopen2([MarshalAs(UnmanagedType.LPStr)] string filename, int flag);
 
+      [DllImport("libdl.so", EntryPoint = "dlerror")]
+      [return: MarshalAs(UnmanagedType.LPStr)]
+      private static extern string dlerror1();
 
-        [DllImport("libdl.so", EntryPoint = "dlclose")]
-        [ReliabilityContract(Consistency.WillNotCorruptState, Cer.Success)]
-        private static extern int dlclose1(IntPtr hModule);
+      [DllImport("libdl.so.2", EntryPoint = "dlerror")]
+      [return: MarshalAs(UnmanagedType.LPStr)]
+      private static extern string dlerror2();
 
-        [DllImport("libdl.so.2", EntryPoint = "dlclose")]
-        [ReliabilityContract(Consistency.WillNotCorruptState, Cer.Success)]
-        private static extern int dlclose2(IntPtr hModule);
+      [DllImport("libdl.so", EntryPoint = "dlclose")]
+      [ReliabilityContract(Consistency.WillNotCorruptState, Cer.Success)]
+      private static extern int dlclose1(IntPtr hModule);
 
+      [DllImport("libdl.so.2", EntryPoint = "dlclose")]
+      [ReliabilityContract(Consistency.WillNotCorruptState, Cer.Success)]
+      private static extern int dlclose2(IntPtr hModule);
 
-        [DllImport("libdl.so", EntryPoint = "dlsym")]
-        private static extern IntPtr dlsym1(IntPtr hModule, [MarshalAs(UnmanagedType.LPStr)] string lpProcName);
-        [DllImport("libdl.so.2", EntryPoint = "dlsym")]
-        private static extern IntPtr dlsym2(IntPtr hModule, [MarshalAs(UnmanagedType.LPStr)] string lpProcName);
-    }
+      [DllImport("libdl.so", EntryPoint = "dlsym")]
+      private static extern IntPtr dlsym1(IntPtr hModule, [MarshalAs(UnmanagedType.LPStr)] string lpProcName);
+
+      [DllImport("libdl.so.2", EntryPoint = "dlsym")]
+      private static extern IntPtr dlsym2(IntPtr hModule, [MarshalAs(UnmanagedType.LPStr)] string lpProcName);
+   }
 }
